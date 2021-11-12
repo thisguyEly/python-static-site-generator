026a7c0fcc5f07eaa0e20d7d8b35b0a1a05f23f8
Ely Sanneman <65604667+thisguyEly@users.noreply.github.com>
2021-11-12 10:29:44 -0500

    Add files via upload

---
+++ ssg.py
@@ -0,0 +1,19 @@
+import ssg.parsers
+import typer
+from ssg.site import Site
+
+
+def main(source="content", dest="dist"):
+    config = {"source": source,
+              "dest": dest,
+              "parsers": [
+                  ssg.parsers.ResourceParser(),
+                  ssg.parsers.MarkdownParser(),
+                  ssg.parsers.ReStructuredTextParser(),
+              ]
+              }
+
+    Site(**config).build()
+
+
+typer.run(main)
