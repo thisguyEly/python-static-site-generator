01c8625fecf4a4a5c9ac54734d7fdbc6fcaa60eb
Ely Sanneman <65604667+thisguyEly@users.noreply.github.com>
2021-11-12 10:02:45 -0500

    Add files via upload

---
+++ ssg/site.py
@@ -0,0 +1,38 @@
+from pathlib import path 
+
+class site:
+    def __init__(self, source, dest, parsers=None):
+        self.source = Path(source)
+        self.dest = Path(dest)
+        self.parsers = parsers or []
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest) 
+
+
+             1848
+        else:
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+                
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    @staticmethod
+    def error(message):
+       sys.stderr.write("\x1b[1;31m{}\n".format(message))
